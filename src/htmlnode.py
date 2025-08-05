057af4b6a2cf712ee125385c6c8d4f8042629d75
Aldo Camacho <me@aldocamacho.com>
2025-08-05 11:53:14 -0700

    add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,3 +30,15 @@ class HTMLNode:
 
         lines.append(")")
         return "\n".join(lines)
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -42,6 +42,22 @@ class TestHTMLNode(unittest.TestCase):
   )
 )""",
         )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a_props(self):
+        node = LeafNode("a", "Hello, world!", {"href": "example.com"})
+        self.assertEqual(node.to_html(), '<a href="example.com">Hello, world!</a>')
+
+    def test_leaf_no_value(self):
+        node = LeafNode("p", None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_leaf_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 
 if __name__ == "__main__":

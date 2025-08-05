cde0da068b09132a1c01cd4a096e09593e85a7e9
Aldo Camacho <me@aldocamacho.com>
2025-08-05 13:44:20 -0700

    add text node to html node function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -32,6 +32,38 @@ class TestTextNode(unittest.TestCase):
     def test_repr(self):
         node = TextNode("Other text node", TextType.LINK, "example.com")
         self.assertEqual(repr(node), "TextNode(Other text node, link, example.com)")
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.props, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.props, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.props, {"href": "example.com"})
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is a text node", TextType.IMAGE, "example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(
+            html_node.props, {"src": "example.com", "alt": "This is a text node"}
+        )
+        self.assertEqual(html_node.value, "")
 
 
 if __name__ == "__main__":

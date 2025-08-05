cea90cd0728b3419de988aff649c9b512afad46d
Aldo Camacho <me@aldocamacho.com>
2025-08-05 15:20:25 -0700

    add function split by delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+import textnode
 
 
 class TestTextNode(unittest.TestCase):
@@ -64,6 +65,76 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
             html_node.props, {"src": "example.com", "alt": "This is a text node"}
         )
         self.assertEqual(html_node.value, "")
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_inline_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_bold(self):
+        node = TextNode("This is **bold** text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" text", TextType.TEXT),
+            ],
+        )
+
+    def test_italic(self):
+        node = TextNode("This is _italic_ text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" text", TextType.TEXT),
+            ],
+        )
+
+    def test_empty_substring(self):
+        node = TextNode("This is __ text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("", TextType.ITALIC),
+                TextNode(" text", TextType.TEXT),
+            ],
+        )
+
+    def test_missing_delimiter(self):
+        node = TextNode("This is **bold text", TextType.TEXT)
+        self.assertRaises(Exception, split_nodes_delimiter, [node], "**", TextType.BOLD)
+
+    def test_multiple_nodes(self):
+        nodes = [
+            TextNode("_italic text_", TextType.TEXT),
+            TextNode("no change", TextType.TEXT),
+            TextNode("**no change**", TextType.ITALIC),
+        ]
+        new_nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("italic text", TextType.ITALIC),
+                TextNode("no change", TextType.TEXT),
+                TextNode("**no change**", TextType.ITALIC),
+            ],
+        )
 
 
 if __name__ == "__main__":
